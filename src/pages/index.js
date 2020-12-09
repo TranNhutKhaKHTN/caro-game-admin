aee212c7ff505e94edaa4ae3da38620f25d8f811
TranNhutKhaKHTN <trannhutkha123456@gmail.com>
2020-12-09 14:16:28 +0700

    aotu rediẻct login

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/login');
+  });
+
   return (
-    <div>Quản lý User</div>
+    <div />
   );
 }
