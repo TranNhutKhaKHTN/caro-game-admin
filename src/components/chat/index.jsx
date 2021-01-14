890e51007cdb74615d1f7b6374260cdcae1be4a9
TranNhutKhaKHTN <trannhutkha123456@gmail.com>
2021-01-14 09:09:19 +0700

    fix

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -7,7 +7,6 @@ const Chat = ({ data }) => (
   <div className={styles.chatWrapper}>
     <Card className={styles.card}>
       <div className={styles.listMess}>
-        {/* eslint-disable-next-line react/no-array-index-key */}
         {data.map((value, index) => <Message key={index} message={value} />)}
       </div>
     </Card>
